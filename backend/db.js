c055e2d545af76c123b3ad38eab6af266d1791c0
Aman Kumar <96868781+Amanraj8969@users.noreply.github.com>
2023-09-13 18:47:48 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,6 @@ const mongoose=require("mongoose")
 
 // require('dotenv').config()
 
-const connection=mongoose.connect("mongodb+srv://aman:12345@cluster0.trvgrs8.mongodb.net/?retryWrites=true&w=majority")
+const connection=mongoose.connect("{enter your mongo url}")
 
 module.exports=connection
